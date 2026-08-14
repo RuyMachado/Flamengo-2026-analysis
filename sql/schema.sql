7a44d6501331e6add5a702256151981d6aa40806
Ruy Machado <ruynetomachado@gmail.com>
2026-08-14 16:21:15 -0300

    feat: adicionado tipo Estadual ao CHECK constraint de competicoes

--- sql/schema.sql
+++ sql/schema.sql
@@ -10,7 +10,7 @@ CREATE TABLE competicoes (
     id SERIAL PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
     temporada SMALLINT NOT NULL,
-    tipo VARCHAR(20) CHECK (tipo IN ('Nacional', 'Continental', 'Internacional'))
+    tipo VARCHAR(20) CHECK (tipo IN ('Nacional', 'Continental', 'Internacional', 'Estadual'))
 );
 
 -- =====================================================================
